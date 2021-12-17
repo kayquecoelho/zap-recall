59d5748d98dd5cc8509c5a08b4f0c1492fa24299
mrcs22 <marcusmoraes2010@hotmail.com>
2021-12-17 15:12:14 -0300

    Preparando revisão

--- src/components/Logo.js
+++
@@ -1,4 +0,0 @@
-import logo from "../assets/logo.png";
-export default function Logo (){
-    return (<img className="logo" src={logo} alt="" />)
-}
--- src/components/LogoMini.js
+++
@@ -1,7 +0,0 @@
-import logoMini from "../assets/logo-mini.png"
-
-export default function LogoMini (){
-    return (
-        <img className="logo-mini" src={logoMini} alt="" />
-    )
-}
